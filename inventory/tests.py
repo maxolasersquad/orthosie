63093671219cf7abd02c49bc77ac117b7595e43f
David Baucum <maxolasersquad@gmail.com>
2012-12-07 20:47:53 -0500

    Began creating some unit tests.

--- inventory/tests.py
+++ inventory/tests.py
@@ -1,16 +1,23 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from inventory.models import Vendor
+from inventory.models import Item
 
+class VendorTest(TestCase):
+    def setUp(self):
+        self.vendor = Vendor(name='Brand X')
+    def test_vendor_name(self):
+        self.assertEqual(self.vendor.name, 'Brand X')
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ItemTest(TestCase):
+    def setUp(self):
+	self.vendor = Vendor(name='Brand X')
+        self.item = Item(upc='12345', name='Product X', vendor=self.vendor)
+
+    def test_item_upc(self):
+        self.assertEqual(self.item.upc, '12345')
+
+    def test_item_name(self):
+        self.assertEqual(self.item.name, 'Product X')
+
+    def test_item_vendor(self):
+        self.assertEqual(self.item.vendor.name, 'Brand X')
--- register/tests.py
+++ register/tests.py
@@ -1,16 +1,6 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from register.models import Transaction
 
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class TransactionTest(TestCase):
+    def setUp(self):
+        self.transaction = Transaction(begin_date=)
