ca381f908a782eec929b4a26b085b053456286bb
David Baucum <david@fink.(none)>
2012-12-10 19:35:41 -0500

    Added more tests.

--- register/tests.py
+++ register/tests.py
@@ -1,6 +1,9 @@
 from django.test import TestCase
 from register.models import Transaction
+from datetime import date
 
 class TransactionTest(TestCase):
     def setUp(self):
-        self.transaction = Transaction(begin_date=)
+        self.transaction = Transaction(begin_date=date(2012,1,1))
+    def test_transaction_begin_date(self):
+        self.assertEqual(self.transaction.begin_date, date(2012,1,1))
