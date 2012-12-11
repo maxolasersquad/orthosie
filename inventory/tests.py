5791b1917db70ff8ccd372c6db301414cd019635
David Baucum <maxolasersquad@gmail.com>
2012-12-11 03:58:51 +0000

    More unit test goodness.

--- inventory/tests.py
+++ inventory/tests.py
@@ -10,14 +10,11 @@ class VendorTest(TestCase):
 
 class ItemTest(TestCase):
     def setUp(self):
-	self.vendor = Vendor(name='Brand X')
+        self.vendor = Vendor(name='Brand X')
         self.item = Item(upc='12345', name='Product X', vendor=self.vendor)
-
     def test_item_upc(self):
         self.assertEqual(self.item.upc, '12345')
-
     def test_item_name(self):
         self.assertEqual(self.item.name, 'Product X')
-
     def test_item_vendor(self):
         self.assertEqual(self.item.vendor.name, 'Brand X')
--- register/tests.py
+++ register/tests.py
@@ -1,9 +1,19 @@
 from django.test import TestCase
 from register.models import Transaction
-from datetime import date
+from inventory.models import Item
+from inventory.models import Vendor
+from datetime import datetime
 
 class TransactionTest(TestCase):
     def setUp(self):
-        self.transaction = Transaction(begin_date=date(2012,1,1))
+        self.vendor = Vendor(name='Brand X')
+        self.item = Item(upc='12345',name='Product X',vendor=self.vendor)
+        self.transaction = Transaction(begin_date=datetime(2012,1,1,8,0,0),finish_date=datetime(2012,1,1,8,7,0),status='Started',item=self.item)
     def test_transaction_begin_date(self):
-        self.assertEqual(self.transaction.begin_date, date(2012,1,1))
+        self.assertEqual(self.transaction.begin_date, datetime(2012,1,1,8,0,0))
+    def test_transaction_finish_date(self):
+        self.assertEqual(self.transaction.finish_date, datetime(2012,1,1,8,7,0))
+    def test_transaction_status(self):
+        self.assertEqual(self.transaction.status, 'Started')
+    def test_transaction_item(self):
+        self.assertEqual(self.transaction.item.name, 'Product X')
