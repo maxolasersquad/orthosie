68105ca50de95d53702c3cb141f73c96daa0f949
David Baucum <maxolasersquad@gmail.com>
2013-03-01 18:54:58 -0500

    Renamed function call in register to view to match new name for transaction totals.

--- register/views.py
+++ register/views.py
@@ -3,6 +3,6 @@ from register.models import *
 
 def index(request):
     line_items = LineItem.objects.order_by('id')
-    transaction_total = line_items[0].transaction.get_transaction_totals()
+    transaction_total = line_items[0].transaction.get_totals()
     context = { 'line_items': line_items }
     return render(request, 'register/index.html', context)
