e2f809e8854a660faec5c2667b19c922239c92d9
David Baucum <maxolasersquad@gmail.com>
2013-03-01 21:01:16 -0500

    Got totals to show up properly on the register.

--- register/views.py
+++ register/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from register.models import *
 
 def index(request):
-    line_items = LineItem.objects.order_by('id')
-    transaction_total = line_items[0].transaction.get_totals()
-    context = { 'line_items': line_items }
+    current_transaction = Transaction.objects.get(finish_date = None)
+    line_items = current_transaction.lineitem_set.all()
+    transaction_total = current_transaction.get_totals()
+    context = { 'line_items': line_items, 'transaction_total': transaction_total }
     return render(request, 'register/index.html', context)
