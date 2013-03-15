5768c8409de75c823c76cdaf79b2a133ab4fd754
David Baucum <maxolasersquad@gmail.com>
2013-03-15 18:59:32 -0400

    Added backspace button.

--- static/js/numpad.js
+++ static/js/numpad.js
@@ -3,7 +3,11 @@ if (Orthosie === undefined) {
 }
 
 Orthosie.numpad = {
-  click: function(key) {
-    $('#input').append(key);
+  append: function(key) {
+  console.log(key);
+    $('#register_input').append(key);
+  },
+  backspace: function() {
+    $('#register_input').html($('#register_input').html().substring(0, $('#register_input').html().length - 1));
   }
 }
